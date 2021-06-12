bbf74f346af31ed1af6067cd14683705693cb7a9
chinglin-k <williamkao2004@gmail.com>
2021-06-12 13:33:09 +0800

    2021.0612.1332

--- 01_02_/Form1.cs
+++ 01_02_/Form1.cs
@@ -1,5 +1,14 @@
 ﻿using System;
 using System.Windows.Forms;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
 
 namespace _01_02_
 {
